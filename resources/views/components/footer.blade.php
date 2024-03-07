d74162ad7bec3fb9ade6045adeaf8f68c951826e
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-03-07 02:40:34 +0100

    Add footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s" style="margin-top: 6rem;">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-4 col-md-6">
                 <h4 class="text-light mb-4">Address</h4>
                 <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
                 <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
@@ -19,30 +19,15 @@
                     </div>
                 @endif
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4">Services</h4>
-                <a class="btn btn-link" href="">Air Freight</a>
-                <a class="btn btn-link" href="">Sea Freight</a>
-                <a class="btn btn-link" href="">Road Freight</a>
-                <a class="btn btn-link" href="">Logistic Solutions</a>
-                <a class="btn btn-link" href="">Industry solutions</a>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4">Quick Links</h4>
+            <div class="col-lg-4 col-md-6">
+                <h4 class="text-light mb-4">Explore</h4>
                 <a class="btn btn-link" href="">Ship</a>
                 <a class="btn btn-link" href="">Track</a>
-                <a class="btn btn-link" href="">Our Services</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
-                <a class="btn btn-link" href="">Support</a>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4">Newsletter</h4>
-                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-                <div class="position-relative mx-auto" style="max-width: 400px;">
-                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                    <button type="button"
-                        class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-                </div>
+            <div class="col-lg-4 col-md-6">
+                <h4 class="text-light mb-4">Legal</h4>
+                <a class="btn btn-link" href="">Terms & Conditions</a>
+                <a class="btn btn-link" href="">Privacy Policy</a>
             </div>
         </div>
     </div>
@@ -50,7 +35,7 @@
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">{{ config('app.name') }}</a>, All Right Reserved.
+                    &copy; <a class="fst-italic" href="#">{{ config('app.name') }}</a> All Right Reserved.
                 </div>
             </div>
         </div>
