383f394f2e4454f9fba0b78eeb7c40350c1e613c
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-03-11 07:29:39 +0100

    Update Font Awesome version and fix image path in CSS file

--- resources/views/components/core-layout.blade.php
+++ resources/views/components/core-layout.blade.php
@@ -15,13 +15,18 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Roboto:wght@500;700&display=swap"
         rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link
+        href="https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css"
+        rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <link href="{{ asset('third-party/animate/animate.min.css') }}" rel="stylesheet">
     <link href="{{ asset('third-party/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}?v={{ time() }}" rel="stylesheet">
+    {{-- @vite('resources/sass/bootstrap.scss') --}}
     <link href="{{ asset('css/style.css') }}?v={{ time() }}" rel="stylesheet">
+
+    @vite('resources/js/app.js')
 </head>
 
 <body>
@@ -66,7 +71,6 @@
     <script src="{{ asset('third-party/waypoints/waypoints.min.js') }}"></script>
     <script src="{{ asset('third-party/counterup/counterup.min.js') }}"></script>
     <script src="{{ asset('third-party/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}?v={{ time() }}"></script>
 </body>
 
 </html>
