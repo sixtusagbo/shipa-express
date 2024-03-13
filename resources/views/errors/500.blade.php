c64e1b2c76427e86be666da9a7f4bbb8e722c43b
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-03-13 05:09:27 +0100

    Update error page messages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,11 +4,11 @@
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
-                    <h1 class="display-1">404</h1>
-                    <h1 class="mb-4">Not Found</h1>
+                    <h1 class="display-1">500</h1>
+                    <h1 class="mb-4">Internal Server Error</h1>
                     <p class="mb-4">
-                        We&apos;re sorry, the page you have looked for does not exist in our website! Go to our
-                        home page?
+                        We&apos;re sorry, we just encountered an error on our end. Rest assured, we'll fix
+                        this soon. Go to our home page?
                     </p>
                     <a class="btn btn-primary rounded-pill py-3 px-5" href="/">Go To Home</a>
                 </div>
